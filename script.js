ae7f2de7ad05a366c2a4141acc21398d5b184dfe
Lynn L <llottman@gmail.com>
2025-11-04 16:42:58 -0500

    add template 5, dark mode

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        const dropdown = document.getElementById('selector');
-        const contentDivs = document.querySelectorAll('.content-div');
-        const storedValue = localStorage.getItem('selectedDropdownValue');
-        
-        if (storedValue) {
-            dropdown.value = storedValue;
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdown = document.getElementById('selector');
+    const contentDivs = document.querySelectorAll('.content-div');
+    const storedValue = localStorage.getItem('selectedDropdownValue');
+    const theme_button = document.getElementById('theme_button')
+    const cssElem = document.querySelector('link[rel="stylesheet"]')
+    let style = localStorage.getItem('selectedStyle');
+    
+    if (storedValue) {
+        dropdown.value = storedValue;
+    }
 
-        dropdown.addEventListener('change', function() {
-            const selectedDivId = this.value;
-            localStorage.setItem('selectedDropdownValue', this.value);
+    if (style) {
+        cssElem.href = style
+    }
 
-            // Hide all content divs
-            contentDivs.forEach(div => {
-                div.style.display = 'none';
-            });
+    dropdown.addEventListener('change', function() {
+        const selectedDivId = this.value;
+        localStorage.setItem('selectedDropdownValue', this.value);
 
-            // Show the selected div
-            const selectedDiv = document.getElementById(selectedDivId);
-            if (selectedDiv) {
-                selectedDiv.style.display = 'block';
-            }
+        // Hide all content divs
+        contentDivs.forEach(div => {
+            div.style.display = 'none';
         });
 
-        // Optional: Trigger change event on page load to show initial div
-        dropdown.dispatchEvent(new Event('change'));
+        // Show the selected div
+        const selectedDiv = document.getElementById(selectedDivId);
+        if (selectedDiv) {
+            selectedDiv.style.display = 'block';
+        }
+
     });
+
+    // Optional: Trigger change event on page load to show initial div
+    dropdown.dispatchEvent(new Event('change'));
+    
+    theme_button.addEventListener('click', function() {
+        if (style == 'otf_style_sheet.css') {
+            style = 'otf_dark_style_sheet.css'
+        } else {
+            style = 'otf_style_sheet.css'
+        }
+        localStorage.setItem('selectedStyle', style);
+        cssElem.href = style;
+    });
+});
